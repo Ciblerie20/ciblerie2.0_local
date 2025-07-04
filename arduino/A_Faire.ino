dfc71ca50d4d8fbdb28b0f434a54d3308401fba1
Ciblerie20 <ciblerie2.0@gmail.com>
2025-07-04 13:36:57 +0200

    Add files via upload

---
+++ arduino/A_Faire.ino
@@ -0,0 +1,13 @@
+
+//\\ PAGE EVOLUTION DU JEU //\\
+
+/*
+scores_screen :
+ ->
+affiché l'heure
+chronomètre total du jeu
+nombre de tir par exemple : 1/12 pour "Classic" et "Compétition" puis sinon pour "LJ" et "MS" le cumul de tir 
+quand "Joueur suivant" clignote , faire idem pour la case du joueur appelé
+pour les tours indiqué le nombre de tour dans le bouton "Tour suivant" par exemple : "1/3"
+
+*/
